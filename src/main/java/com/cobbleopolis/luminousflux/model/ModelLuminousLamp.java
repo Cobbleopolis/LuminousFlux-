b71d9b47ceeba591a9da90dd1f2b90adb5b66553
Cobbleopolis <agounimon@yahoo.com>
2015-02-11 18:35:19 -0500

    Reworked the Lamp model

--- src/main/java/com/cobbleopolis/luminousflux/model/ModelLuminousLamp.java
+++ src/main/java/com/cobbleopolis/luminousflux/model/ModelLuminousLamp.java
@@ -9,49 +9,59 @@ import net.minecraft.entity.Entity;
  * Created using Tabula 4.1.1
  */
 public class ModelLuminousLamp extends ModelBase {
-    public ModelRenderer pillar4;
-    public ModelRenderer pillar3;
-    public ModelRenderer pillar2;
     public ModelRenderer bottom;
     public ModelRenderer pillar1;
+    public ModelRenderer pillar2;
+    public ModelRenderer pillar3;
+    public ModelRenderer pillar4;
     public ModelRenderer top;
-    public ModelRenderer top_connector;
+    public ModelRenderer top2;
+    public ModelRenderer top3;
+    public ModelRenderer center;
 
     public ModelLuminousLamp() {
-        this.textureWidth = 64;
-        this.textureHeight = 35;
-        this.bottom = new ModelRenderer(this, 0, 0);
-        this.bottom.setRotationPoint(-8.0F, 23.0F, -8.0F);
-        this.bottom.addBox(0.0F, 0.0F, 0.0F, 16, 1, 16, 0.0F);
-        this.pillar2 = new ModelRenderer(this, 0, 0);
-        this.pillar2.setRotationPoint(7.0F, 9.0F, -8.0F);
-        this.pillar2.addBox(0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F);
-        this.top_connector = new ModelRenderer(this, 0, 17);
-        this.top_connector.setRotationPoint(-1.0F, -8.0F, -1.0F);
-        this.top_connector.addBox(0.0F, 0.0F, 0.0F, 2, 16, 2, 0.0F);
-        this.pillar3 = new ModelRenderer(this, 0, 0);
-        this.pillar3.setRotationPoint(7.0F, 9.0F, 7.0F);
-        this.pillar3.addBox(0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F);
-        this.pillar4 = new ModelRenderer(this, 0, 0);
-        this.pillar4.setRotationPoint(-8.0F, 9.0F, 7.0F);
-        this.pillar4.addBox(0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F);
+        this.textureWidth = 36;
+        this.textureHeight = 32;
         this.top = new ModelRenderer(this, 0, 0);
-        this.top.setRotationPoint(-8.0F, 8.0F, -8.0F);
-        this.top.addBox(0.0F, 0.0F, 0.0F, 16, 1, 16, 0.0F);
-        this.pillar1 = new ModelRenderer(this, 0, 0);
-        this.pillar1.setRotationPoint(-8.0F, 9.0F, -8.0F);
-        this.pillar1.addBox(0.0F, 0.0F, 0.0F, 1, 14, 1, 0.0F);
+        this.top.setRotationPoint(-4.0F, 12.0F, -4.0F);
+        this.top.addBox(0.0F, 0.0F, 0.0F, 8, 1, 8, 0.0F);
+        this.top3 = new ModelRenderer(this, 0, 16);
+        this.top3.setRotationPoint(-2.0F, 10.0F, -2.0F);
+        this.top3.addBox(0.0F, 0.0F, 0.0F, 4, 1, 4, 0.0F);
+        this.pillar4 = new ModelRenderer(this, 32, 0);
+        this.pillar4.setRotationPoint(-4.0F, 13.0F, 3.0F);
+        this.pillar4.addBox(0.0F, 0.0F, 0.0F, 1, 10, 1, 0.0F);
+        this.pillar3 = new ModelRenderer(this, 32, 0);
+        this.pillar3.setRotationPoint(3.0F, 13.0F, 3.0F);
+        this.pillar3.addBox(0.0F, 0.0F, 0.0F, 1, 10, 1, 0.0F);
+        this.pillar2 = new ModelRenderer(this, 32, 0);
+        this.pillar2.setRotationPoint(-4.0F, 13.0F, -4.0F);
+        this.pillar2.addBox(0.0F, 0.0F, 0.0F, 1, 10, 1, 0.0F);
+        this.center = new ModelRenderer(this, 10, 16);
+        this.center.setRotationPoint(-3.0F, 13.0F, -3.0F);
+        this.center.addBox(0.0F, 0.0F, 0.0F, 6, 10, 6, 0.0F);
+        this.top2 = new ModelRenderer(this, 0, 9);
+        this.top2.setRotationPoint(-3.0F, 11.0F, -3.0F);
+        this.top2.addBox(0.0F, 0.0F, 0.0F, 6, 1, 6, 0.0F);
+        this.pillar1 = new ModelRenderer(this, 32, 0);
+        this.pillar1.setRotationPoint(3.0F, 13.0F, -4.0F);
+        this.pillar1.addBox(0.0F, 0.0F, 0.0F, 1, 10, 1, 0.0F);
+        this.bottom = new ModelRenderer(this, 0, 0);
+        this.bottom.setRotationPoint(-4.0F, 23.0F, -4.0F);
+        this.bottom.addBox(0.0F, 0.0F, 0.0F, 8, 1, 8, 0.0F);
     }
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.bottom.render(f5);
-        this.pillar2.render(f5);
-//        this.top_connector.render(f5);
-        this.pillar3.render(f5);
-        this.pillar4.render(f5);
         this.top.render(f5);
+        this.top3.render(f5);
+        this.pillar4.render(f5);
+        this.pillar3.render(f5);
+        this.pillar2.render(f5);
+        this.center.render(f5);
+        this.top2.render(f5);
         this.pillar1.render(f5);
+        this.bottom.render(f5);
     }
 
     /**
--- src/main/java/com/cobbleopolis/luminousflux/renderer/RendererLuminousLamp.java
+++ src/main/java/com/cobbleopolis/luminousflux/renderer/RendererLuminousLamp.java
@@ -2,18 +2,13 @@ package com.cobbleopolis.luminousflux.renderer;
 
 import com.cobbleopolis.luminousflux.LuminousFlux;
 import com.cobbleopolis.luminousflux.model.ModelLuminousLamp;
-import com.cobbleopolis.luminousflux.reference.Textures;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntityChestRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
 import net.minecraftforge.client.IItemRenderer;
 import org.lwjgl.opengl.GL11;
 
@@ -55,30 +50,36 @@ public class RendererLuminousLamp extends TileEntitySpecialRenderer implements
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
 		GL11.glPushMatrix();
-		GL11.glTranslated(0F, -0.5F, 0F);
+		GL11.glCullFace(GL11.GL_FRONT);
+		GL11.glScalef(1F, -1F, 1F);
+		GL11.glTranslatef(0F, -0.5F, 0F);
 		switch (type) {
 			case INVENTORY:
-				GL11.glTranslated(0F, -0.5F, 0F);
+				GL11.glTranslatef(0F, -0.5F, 0F);
 			default:
 		}
 		ResourceLocation textures = (new ResourceLocation(
 				LuminousFlux.MODID + ":textures/blocks/lamp.png"));
 		Minecraft.getMinecraft().renderEngine.bindTexture(textures);
 		this.model.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+		GL11.glCullFace(GL11.GL_BACK);
 		GL11.glPopMatrix();
 	}
 
 	@Override
 	public void renderTileEntityAt(TileEntity te, double x, double y,
 								   double z, float paramFloat) {
+		float f5 = 0.0625F;
 		GL11.glPushMatrix();
-//        GL11.glRotatef(180F, 1f, 0f, 0f);
-		GL11.glTranslatef((float) x + 0.5F, (float) y - 0.5F, (float) z + 0.5F);
+		GL11.glCullFace(GL11.GL_FRONT);
+		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+		GL11.glScalef(1F, -1F, 1F);
 		ResourceLocation textures = (new ResourceLocation(
-				 LuminousFlux.MODID + ":textures/blocks/lamp.png"));
+				LuminousFlux.MODID + ":textures/blocks/lamp.png"));
 		Minecraft.getMinecraft().renderEngine.bindTexture(textures);
 		GL11.glPushMatrix();
 		this.model.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+		GL11.glCullFace(GL11.GL_BACK);
 		GL11.glPopMatrix();
 		GL11.glPopMatrix();
 	}
