21c727b73b7f660fd19e97c05e1f7f40f7a3a411
Cobbleopolis <cobbleopolis@gmail.com>
2015-03-16 12:28:09 -0400

    Fixed transparency issue with the light block

--- src/main/java/com/cobbleopolis/luminousflux/renderer/RendererLight.java
+++ src/main/java/com/cobbleopolis/luminousflux/renderer/RendererLight.java
@@ -46,14 +46,12 @@ public class RendererLight extends TileEntitySpecialRenderer implements
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
 		GL11.glPushMatrix();
 		GL11.glCullFace(GL11.GL_FRONT);
-		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glScalef(1F, -1F, 1F);
 		GL11.glTranslatef(0F, -1F, 0F);
 		ResourceLocation textures = (new ResourceLocation(
 				LuminousFlux.MODID + ":textures/blocks/light2.png"));
 		Minecraft.getMinecraft().renderEngine.bindTexture(textures);
 		this.model.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glCullFace(GL11.GL_BACK);
 		GL11.glPopMatrix();
 	}
@@ -63,48 +61,47 @@ public class RendererLight extends TileEntitySpecialRenderer implements
 								   double z, float paramFloat) {
 		TileEntityLight te = (TileEntityLight) tileEntity;
 		int dir = te.direction;
-//		System.out.println(dir);
 		GL11.glPushMatrix();
 		GL11.glCullFace(GL11.GL_FRONT);
-		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-		switch (dir) {
-			case 1: //WEST
-				GL11.glRotatef(-90F, 0F, 0F, 1F);
-				GL11.glTranslatef(1F, 1F, 0F);
-				break;
-			case 2: //EAST
-				GL11.glRotatef(90F, 0F, 0F, 1F);
-				GL11.glTranslatef(-1F, 1F, 0F);
-				break;
-			case 3: //NORTH
-				GL11.glRotatef(90F, 1F, 0F, 0F);
-				GL11.glTranslatef(0F, 1F, 1F);
-				break;
-			case 4: //SOUTH
-				GL11.glRotatef(-90F, 1F, 0F, 0F);
-				GL11.glTranslatef(0F, 1F, -1F);
-				break;
-			case 5: //DOWN
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+        switch (dir) {
+            case 1: //WEST
+                GL11.glRotatef(-90F, 0F, 0F, 1F);
+                GL11.glTranslatef(1F, 1F, 0F);
+                break;
+            case 2: //EAST
+                GL11.glRotatef(90F, 0F, 0F, 1F);
+                GL11.glTranslatef(-1F, 1F, 0F);
+                break;
+            case 3: //NORTH
+                GL11.glRotatef(90F, 1F, 0F, 0F);
+                GL11.glTranslatef(0F, 1F, 1F);
+                break;
+            case 4: //SOUTH
+                GL11.glRotatef(-90F, 1F, 0F, 0F);
+                GL11.glTranslatef(0F, 1F, -1F);
+                break;
+            case 5: //DOWN
 
-				break;
-			case 6: //UP
-				GL11.glRotatef(-180F, 1F, 0F, 0F);
-				GL11.glTranslatef(0F, 2F, 0F);
-				break;
-		}
-		GL11.glScalef(1F, -1F, 1F);
-		ResourceLocation textures = (new ResourceLocation(
+                break;
+            case 6: //UP
+                GL11.glRotatef(-180F, 1F, 0F, 0F);
+                GL11.glTranslatef(0F, 2F, 0F);
+                break;
+        }
+        GL11.glScalef(1F, -1F, 1F);
+        ResourceLocation textures = (new ResourceLocation(
 				LuminousFlux.MODID + ":textures/blocks/light2.png"));
-		Minecraft.getMinecraft().renderEngine.bindTexture(textures);
-		GL11.glPushMatrix();
-		this.model.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-		if (!te.bulbItem.equalsIgnoreCase("none")) {
-
-			this.model.light_bottom.render(0.0625F);
-			this.model.light_top.render(0.0625F);
-		}
-		GL11.glCullFace(GL11.GL_BACK);
-		GL11.glPopMatrix();
+        Minecraft.getMinecraft().renderEngine.bindTexture(textures);
+        this.model.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+        if (!te.bulbItem.equalsIgnoreCase("none")) {
+            this.model.light_bottom.render(0.0625F);
+            this.model.light_top.render(0.0625F);
+        }
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glCullFace(GL11.GL_BACK);
 		GL11.glPopMatrix();
 	}
 }
