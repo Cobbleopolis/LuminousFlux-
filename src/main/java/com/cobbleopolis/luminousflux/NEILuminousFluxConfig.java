e46af0e29ea5ad039ed0b7b59e011b6bd2aeb07b
Cobbleopolis <agounimon@yahoo.com>
2015-03-22 20:16:27 -0400

    Started NEI Integration

---
+++ src/main/java/com/cobbleopolis/luminousflux/NEILuminousFluxConfig.java
@@ -0,0 +1,20 @@
+package com.cobbleopolis.luminousflux;
+
+import codechicken.nei.api.IConfigureNEI;
+
+public class NEILuminousFluxConfig implements IConfigureNEI {
+	@Override
+	public void loadConfig() {
+
+	}
+
+	@Override
+	public String getName() {
+		return LuminousFlux.MODID;
+	}
+
+	@Override
+	public String getVersion() {
+		return null;
+	}
+}
