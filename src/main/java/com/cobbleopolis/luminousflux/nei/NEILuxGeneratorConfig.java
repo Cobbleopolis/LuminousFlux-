0958a4fbdef3b743868b095dde9a6d83929100d3
Cobbleopolis <agounimon@yahoo.com>
2015-03-30 17:14:54 -0400

    Work on Solar Generator

--- src/main/java/com/cobbleopolis/luminousflux/nei/NEILuxGeneratorConfig.java
+++ src/main/java/com/cobbleopolis/luminousflux/nei/NEILuxGeneratorConfig.java
@@ -1,4 +1,18 @@
 package com.cobbleopolis.luminousflux.nei;
 
 public class NEILuxGeneratorConfig {
+//	@Override
+//	public void loadConfig() {
+//
+//	}
+//
+//	@Override
+//	public String getName() {
+//		return LuminousFlux.MODID;
+//	}
+//
+//	@Override
+//	public String getVersion() {
+//		return LuminousFlux.VERSION;
+//	}
 }
--- src/main/java/com/cobbleopolis/luminousflux/tileentity/TileEntitySolarLuxGenerator.java
+++ src/main/java/com/cobbleopolis/luminousflux/tileentity/TileEntitySolarLuxGenerator.java
@@ -1,5 +1,39 @@
 package com.cobbleopolis.luminousflux.tileentity;
 
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumSkyBlock;
+
 public class TileEntitySolarLuxGenerator extends TileEntityLuxPowered {
+
+	@Override
+	public void updateEntity() {
+		super.updateEntity();
+//		int lightLevel = worldObj.getFullBlockLightValue(xCoord, yCoord + 1, zCoord);
+//		System.out.println(worldObj.getCelestialAngle(1.0F));
+//		if(worldObj.canBlockSeeTheSky(xCoord, yCoord, zCoord) && worldObj.getCelestialAngle(1.0F) < (float) Math.PI) {
+//			if (this.storedLux < this.maxLux)
+//				this.storedLux = Math.min((int)(this.storedLux + Math.ceil((double) lightLevel / 10)), this.maxLux);
+//		}
+		if (!worldObj.provider.hasNoSky) {
+			int lightLevel = worldObj.getSavedLightValue(EnumSkyBlock.Sky, xCoord, yCoord, zCoord) - worldObj.skylightSubtracted;
+			float skyRotation = worldObj.getCelestialAngleRadians(1.0F);
+
+			if (skyRotation < (float)Math.PI) {
+				skyRotation += (0.0F - skyRotation) * 0.2F;
+			} else {
+				skyRotation += (((float)Math.PI * 2F) - skyRotation) * 0.2F;
+			}
+
+			lightLevel = Math.round((float)lightLevel * MathHelper.cos(skyRotation));
+
+			lightLevel = Math.max(lightLevel, 0);
+			lightLevel = Math.min(lightLevel, 15);
+
+//			if (this.storedLux < this.maxLux)
+				this.storedLux = Math.min((int)(this.storedLux + Math.ceil((double) lightLevel / 10)), this.maxLux);
+
+		}
+
+	}
 
 }
